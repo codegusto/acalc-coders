da0de85288d2a6b11841f59779f8d0f523512333
Robert <robert.williams@codegusto.com>
2018-10-04 12:48:32 -0500

    Week 1 file upload
    Code examples for Week 1 of ACALC Coders.  Examples files taken from Teach Your Kids to Code by Bryson Payne.

---
+++ Week1/MadLib.py
@@ -0,0 +1,6 @@
+#MadLib.py
+adjective = input("Please enter an adjective [examples: smart, big, purple, or grumpy]: ")
+noun = input("Please enter a noun [examples: girl, coyote, teacher] : ")
+verb = input("Please enter a verb ending in -ed [examples: walked, jumped, rolled]: ")
+print("Your MadLib:")
+print("The", adjective, noun, verb, "over the lazy brown dog.")
---
+++ Week1/MadLib2.py
@@ -0,0 +1,7 @@
+#MadLib.py
+adjective = input("Please enter an adjective [examples: smart, big, purple, or grumpy]: ")
+noun = input("Please enter a noun [examples: girl, paperclip, teacher]: ")
+verb = input("Please enter a verb ending in -ed [examples: walked, jumped, rolled]: ")
+animal = input("Please enter a type of animal [examples: coyote, bear, moose]: ")
+print("Your MadLib:")
+print("The", adjective, noun, verb, "over the lazy brown", animal)
