ed29e444ea15dc0c74ebc71f72e4304104d63e3a
Robert <robert.williams@codegusto.com>
2018-10-25 15:35:31 -0500

    Week3

---
+++ Week3/ThankYou.py
@@ -0,0 +1,7 @@
+my_name = "Mr. Computer"
+my_age = 99
+your_name = input("What is your name? ")
+your_age = input("How old are you? ")
+print("My name is", my_name, ", and I am", my_age, "years old.")
+print("Your name is", your_name, ", and you are", your_age, ".")
+print("Thank you for visiting with me,", your_name, "!")
